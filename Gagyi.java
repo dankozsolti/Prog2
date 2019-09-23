cdb3db90d3614fd2d57ddf3f887ea6b4eced8d62
dankozsolti <43618376+dankozsolti@users.noreply.github.com>
2019-09-23 12:43:59 +0200

    Update Gagyi.java

--- Gagyi.java
+++ Gagyi.java
@@ -1,30 +1,28 @@
 package gagyi;
 
-import static java.lang.Thread.sleep;
+import java.lang.Thread;
 
 public class Gagyi {
 
     public static void main(String[] args) throws InterruptedException {
-       Integer x = 127; //Integer.valueOf(127)
-       Integer t = 127; //Integer.valueOf(127) ugyanarra hivatkozik
+       Integer x = 127;
+       Integer t = 127;
        
-       System.out.println(x == t); //emiatt biztosan nem lép be az 1 whileba
+       System.out.println(x == t);
 
        
        while (x <= t && x >= t && t != x){
            System.out.println(1); 
-           //nem lép be a ciklusba
        }
        
        x = 128;
        t = 128;
        
-       System.out.println(x == t); //a két hivatkozás már nem egyezik meg
+       System.out.println(x == t);
        
        while (x <= t && x >= t && t != x){
-           System.out.println("VÉGTELEEEEN");   //belép, mert a 128 már nincs 
-           sleep(3000);                 // poolozva, igy a két értéknek 2 külön címe lesz,
-                                        // azaz nem egyenlőek
+           System.out.println("Végtelen ciklus");
+           Thread.sleep(3000);
        }
     
     }
